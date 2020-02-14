fc7680c0f939b46c2c559ca3750740d35b773269
Joungho Seo <js6108@rit.edu>
2020-02-13 20:47:13 -0500

    Updated Equipment Class, still unfinished

--- Equipment.java
+++ Equipment.java
@@ -4,4 +4,50 @@
  *
  * */
 
+public class Equipment {
+  private static int id;
+  private static String name;
+  private static String desc;
+  private static int cap;
 
+  Equipment() {
+
+  }
+
+  Equipment(int inputID){
+    id = inputID;
+  }
+
+  Equipment(int inputID, String inputName, String inputDesc, int inputCap){
+    id = inputID;
+    name = inputName;
+    desc = inputDesc;
+    cap = inputCap;
+  }
+
+  public int getID() {
+    return id;
+  }
+  public String getName() {
+    return name;
+  }
+  public String getDesc() {
+    return desc;
+  }
+  public int getDesc() {
+    return cap;
+  }
+  public void setID(int input) {
+    id=input;
+  }
+  public void getName(String input) {
+    name=input;
+  }
+  public void getDesc(String input) {
+    desc=input;
+  }
+  public void getDesc(String input) {
+    cap=input;
+  }
+  public static
+}
